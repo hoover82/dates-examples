c759e23a97d71c309fdcaec54299b65e4d1688b8
hoover82 <33789565+hoover82@users.noreply.github.com>
2018-02-07 12:00:49 -0700

    Add files via upload

--- calendar.py
+++ calendar.py
@@ -1,5 +1,4 @@
 
-
 from datetime import datetime, date, time
 import datetime
 #from dateutil.relativedelta import *
@@ -7,6 +6,7 @@ import datetime
 output = []
 
 days_header = ['Su','Mo','Tu','We','Th','Fr','Sa']
+months_list = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 mo_str = input ("\nEnter Month (format MM): ")
 yr_str = input (  "Enter Year (format YYYY):")
@@ -20,14 +20,24 @@ d = datetime.datetime(int(yr_str),int(mo_str),1)
 day1_dow = 0 if datetime.datetime.weekday(d)== 6 else datetime.datetime.weekday(d)+1
 
 #last_day = d + dateutil.relativedelta(day=1, months=+1, days=-1)
+#dan = d + datetime.timedelta (months=+1,days=-1) 
+last_day = datetime.datetime(int(yr_str)+1, 1 ,1) + datetime.timedelta (days=-1) if mo_str == "12" else datetime.datetime(int(yr_str),int(mo_str)+1 ,1) + datetime.timedelta (days=-1)
+last_day_number = int( last_day.strftime ( '%d'))
 
+#print ( "Last Day of month is {0:2d}".format(last_day_number))
+
+#Fills an array of the 42 possible sqaures ont he calendar
 for i in range (42):
 
-    if i< day1_dow:
+    if i< day1_dow or (i+1-day1_dow) > last_day_number:
         output.append ("**")
     else:
         output.append ( "{0:2d}".format(i+1-day1_dow))
 
+		
+print ( "\n      "+months_list[int ( mo_str)-1]+" {0:%Y}".format(d))
+		
+		
 print ( days_header[0] + " "
       + days_header[1] + " "
       + days_header[2] + " "
